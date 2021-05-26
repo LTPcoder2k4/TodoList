10a702a9e9bba22fbe434d64a2cbf59aa5fddf31
Lê Tấn Phát <84580263+LTPcoder2k4@users.noreply.github.com>
2021-05-27 00:23:07 +0700

    Delete ToDoList directory

--- ToDoList/Input.py
+++
@@ -1,8 +0,0 @@
-inp_helper = """
-MDTextField:
-    hint_text: "Name: "
-    icon_right: "location-enter"
-    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-    size_hint_x: None
-    width: 300
-"""
